6d8129b676a0fa8e8ebfefeb6ffdc076435a399f
pozolitoxd <estebantoro.p.7@gmail.com>
2025-08-19 12:14:34 -0600

    Implemetanción de más routers

--- backend/router/router.categorias.js
+++ backend/router/router.categorias.js
@@ -4,12 +4,9 @@ const router = express.Router()
 const controllerCategorias = require('../controller/controller.categorias')
 const validatorApiKey = require('../apiKey/apikey')
 
-
-//app.use('/categorias',validatorApiKey)
-
-//router.get('/categorias',controllerCategorias.getCategoria)
-//router.post('/categorias',controllerCategorias.insertCategoria)
-//router.put('/categorias/:id',controllerCategorias.updateCategoria)
-//router.delete('/categorias/:id',controllerCategorias.deleteCategoria)
+router.get('/categorias',validatorApiKey,controllerCategorias.getCategoria)
+router.post('/categorias',validatorApiKey,controllerCategorias.insertCategoria)
+router.put('/categorias/:id',validatorApiKey,controllerCategorias.updateCategoria)
+router.delete('/categorias/:id',validatorApiKey,controllerCategorias.deleteCategoria)
 
 module.exports=router
